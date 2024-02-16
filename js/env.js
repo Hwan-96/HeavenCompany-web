67dc0a1b96b32946afa7741a880dcf0ca7c94884
Hwan-96 <141700374+Hwan-96@users.noreply.github.com>
2024-02-16 10:54:25 +0900

    map20240216

--- js/env.js
+++ js/env.js
@@ -1,3 +1,4 @@
 const API_KEY = 'AIzaSyBaPsRZprCpIa8k6xzZlgExTd087NSkTUo';
+// 나중에 회사 키로 발급 받아서 변경필요
 
-// export { API_KEY };
+export { API_KEY };
--- js/map.js
+++ js/map.js
@@ -1,12 +1,163 @@
-// import { API_KEY  } from "./env";
+import { API_KEY  } from "./env.js";
+
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap&region=kr`;
+script.defer = true;
+document.head.appendChild(script);
 
 window.initMap = function () {
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 37.5400456, lng: 126.9921017 },
-    zoom: 10,
+    // center: { lat: 37.5400456, lng: 126.9921017 },
+    center: { lat: 37.5213443, lng: 127.0462639 },
+    zoom: 18,
+    disableDefaultUI: true,
+    styles: [
+      {
+          "featureType": "all",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#202c3e"
+              }
+          ]
+      },
+      {
+          "featureType": "all",
+          "elementType": "labels.text.fill",
+          "stylers": [
+              {
+                  "gamma": 0.01
+              },
+              {
+                  "lightness": 20
+              },
+              {
+                  "weight": "1.39"
+              },
+              {
+                  "color": "#ffffff"
+              }
+          ]
+      },
+      {
+          "featureType": "all",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+              {
+                  "weight": "0.96"
+              },
+              {
+                  "saturation": "9"
+              },
+              {
+                  "visibility": "on"
+              },
+              {
+                  "color": "#000000"
+              }
+          ]
+      },
+      {
+          "featureType": "all",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": 30
+              },
+              {
+                  "saturation": "9"
+              },
+              {
+                  "color": "#29446b"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "saturation": 20
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": 20
+              },
+              {
+                  "saturation": -20
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": 10
+              },
+              {
+                  "saturation": -30
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#193a55"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "geometry.stroke",
+          "stylers": [
+              {
+                  "saturation": 25
+              },
+              {
+                  "lightness": 25
+              },
+              {
+                  "weight": "0.01"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": -20
+              }
+          ]
+      }
+  ]
   });
-};
 
-// const googleMap = () => {
-//   const URL =`https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
-// }
+  new google.maps.Marker({
+    position: { lat: 37.5213443, lng: 127.0462639 },
+    map: map,
+    // label: "헤븐컴퍼니",
+    icon: {
+      url: '../images/LOGO_S.png',
+      scaledSize: new google.maps.Size(40, 60),
+    },
+  });
+
+};
